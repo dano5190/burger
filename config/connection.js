97ec20c23e25c6158adc4f47b9125b34995628b7
dark blue <29714452+dano5190@users.noreply.github.com>
2019-01-28 09:17:09 -0800

    Changed the connection.js with the JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var conn;
 
-var conn = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+   conn = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+ conn = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burger_db"
 });
+};
 
 conn.connect(function(err){
     if(err){
